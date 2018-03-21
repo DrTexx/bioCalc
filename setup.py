4690af8c2ac1aa54386488c0fced9d9b90a71d12
Denver P <23412100+DrTexxOfficial@users.noreply.github.com>
2018-03-21 11:49:23 +1100

    wip changes for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'bioCalc',
+  packages = ['bioCalc'], # this must be the same as the name above
+  version = '0.1',
+  description = 'biological calculator',
+  author = 'Denver P.',
+  author_email = 'DrTexxOfficial@gmail.com',
+  url = 'https://github.com/DrTexxOfficial/bioCalc', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
