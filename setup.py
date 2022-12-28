736d007a1ae285b8eb576fc96587604c231a3cb7
DrTexx <23412100+DrTexx@users.noreply.github.com>
2022-12-29 10:13:47 +1100

    pin dbi req. version + add note about new owner

--- setup.py
+++ setup.py
@@ -122,7 +122,10 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['dbi'],  # Optional
+    install_requires=[
+        # NOTE: ! **DO NOT UNPIN THIS REQ VERSION** ! - PACKAGE NAMESPACE HAS NEW OWNER
+        'dbi==1.2.5'
+    ],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
